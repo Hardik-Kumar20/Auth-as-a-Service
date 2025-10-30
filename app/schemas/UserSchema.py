a3df3e5562de2c190d69ae55a2f0b509a60659ae
Hardik kumar <kumarhardik821@gmail.com>
2025-10-30 12:57:09 -0700

    New files

---
+++ app/schemas/UserSchema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Emailstr
+
+class UserCreate(BaseModel):
+    email: Emailstr
+    password: str
+
+
+class UserLogin(BaseModel):
+    password: str
+    email: Emailstr
+
+
+class UserOut(BaseModel):
+    
